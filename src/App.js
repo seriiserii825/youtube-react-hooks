64a4fa1cdcb3178fb12c4c6ee0c302676711ff4b
serii <seriiburduja@gmail.com>
2021-08-11 13:51:18 +0300

    todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Context } from "./Context";
 import TodoList from './TodoList'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputTodo, setInputTodo] = useState('');
+
+  const addTodo = (e, todo) => {
+    if (inputTodo !== '' && e.key === 'Enter') {
+      setTodos([todo, ...todos]);
+      setInputTodo('');
+    }
   }
 
-  render() {
-    return (
+  useEffect(function getFromLocalStorage() {
+    const localTodos = localStorage.getItem('todos') || [];
+    setTodos(JSON.parse(localTodos))
+  }, []);
+
+  useEffect(function addToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const toggleTodoCompleted = (id) => {
+    const todosChanged = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    });
+    setTodos([...todosChanged]);
+  }
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  const value = { toggleTodoCompleted, removeTodo };
+
+  return (
+    <Context.Provider value={value}>
       <div className="container">
         <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+        <div className="input-field">
+          <input type="text" value={inputTodo} onChange={(e) => setInputTodo(e.target.value)}
+                 onKeyDown={(e) => addTodo(e, { id: +new Date(), title: inputTodo, completed: false })}/>
+          <label>Todo name</label>
+        </div>
 
-          <TodoList todos={this.state.todos} />
+        <TodoList todos={todos}/>
       </div>
-    );
-  }
+    </Context.Provider>
+  );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from "./Context";
 
-export default function TodoItem({title, id, completed}) {
+export default function TodoItem({ title, id, completed }) {
+  const { toggleTodoCompleted, removeTodo } = useContext(Context);
+  const cls = ['todo'];
+  if (completed) {
+    cls.push('completed');
+  }
   return (
-    <li className="todo">
+    <li className={cls.join(' ')}>
       <label>
         <input
           type="checkbox"
-          defaultChecked={false}
+          checked={completed}
+          onChange={() => {
+            console.log(id, 'id')
+            toggleTodoCompleted(id)
+          }}
         />
         <span>{title}</span>
-
         <i
           className="material-icons red-text"
+          onClick={() => removeTodo(id)}
         >
           delete
         </i>
